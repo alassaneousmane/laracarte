fa05b53e9b9184aef75d0e72c390abcc6eb43893
TOURE, AA <ousmanealassane.toure@gmail.com>
2018-12-01 19:33:43 +0100

    Delete the strong balise

--- resources/views/layouts/partials/_footer.blade.php
+++ resources/views/layouts/partials/_footer.blade.php
@@ -2,7 +2,7 @@
 <div class="container text-center">
 <div class="row">
 <div class="col-lg-12">
-<p>&copy; {{ date('Y') }} &middot; Laracarte by <a href="//twitter.com/etsmo">Honore Hounwanou</a> &dash; A <a href="//laramap.com">Laramap</a> clone app.</p><p><strong>This app has been built for learning purposes.</strong></p>     
+<p>&copy; {{ date('Y') }} &middot; Laracarte by <a href="//twitter.com/etsmo">Honore Hounwanou</a> &dash; A <a href="//laramap.com">Laramap</a> clone app.</p><p>This app has been built for learning purposes.</p>     
 
 </div>
 </div>    
