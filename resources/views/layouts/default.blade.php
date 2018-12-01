de3d9ae5e9422e1edd6dc382daf0ce4afa728fca
TOURE, AA <ousmanealassane.toure@gmail.com>
2018-12-01 19:44:54 +0100

    Add page title

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -3,7 +3,8 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<title>Laravel</title>
+	
+	<title> {{ page_title($title ?? '') }} Laracarte - List of artisans</title>
 
 	{{-- Google Fonts --}}
 	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,5 +1,5 @@
-@extends('layouts.default')
+@extends('layouts.default', ['title' => 'Home'])
 
 @section('content')
-<p>Home</p>
+<h1 class="text-center">Map here !!!!!!</h1>
 @stop
