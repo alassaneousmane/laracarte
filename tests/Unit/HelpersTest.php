c6cdd0889fa56bacdc015b1298be26495569b53f
TOURE, AA <ousmanealassane.toure@gmail.com>
2018-12-08 19:35:45 +0100

    Tests unitaires

--- tests/Unit/HelpersTest.php
+++ tests/Unit/HelpersTest.php
@@ -25,4 +25,20 @@ class HelpersTest extends TestCase
     	$this->assertEquals('About | Laracarte - List of artisans', page_title('About'));
     	$this->assertEquals('Home | Laracarte - List of artisans', page_title('Home'));
     }
+
+
+    /**
+    * @test
+    **/
+    public function set_active_should_return_the_correct_class_based_on_a_given_route() {
+        $this->get(route('home'));
+        $this->assertEquals('active', set_active_route('home'));
+        $this->assertEquals('', set_active_route('about'));
+
+        // Je me rends dans route_path
+        $this->get(route('contact'));
+        $this->assertEquals('', set_active_route('home'));
+        $this->assertEquals('', set_active_route('about'));
+        $this->assertEquals('active', set_active_route('contact'));
+    }
 }
