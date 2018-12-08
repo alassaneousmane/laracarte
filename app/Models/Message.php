8d4d4fbe59286ee6f0982068680656b1075606ac
TOURE, AA <ousmanealassane.toure@gmail.com>
2018-12-08 19:36:57 +0100

    Add scope to fetch 2 months old messages to delete with command

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -2,9 +2,14 @@
 
 namespace App\Models;
 
+use App\Scopes\DateScopable;
 use Illuminate\Database\Eloquent\Model;
 
 class Message extends Model
 {
+	use DateScopable;
+	
     protected $fillable = ['name', 'email', 'message'];
+
+
 }
