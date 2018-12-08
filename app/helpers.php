823d405e71e0c4287147ac79ffc54f75a5fc2234
TOURE, AA <ousmanealassane.toure@gmail.com>
2018-12-08 19:37:46 +0100

    Add method to count messages saved in DB

--- app/helpers.php
+++ app/helpers.php
@@ -1,19 +1,29 @@
 <?php 
 
+use App\Models\Message;
+
 
 if(!function_exists('page_title')) {
 	function page_title($title) {
 		$base_title = config('app.name'). ' - List of artisans';
-		if($title === '') {
-			return $base_title;
-		} else {
-			return $title . ' | '. $base_title;
-		}
+
+		return empty($title) ? $base_title : sprintf('%s | %s', $title, $base_title);
+		
 	}
 }
 
 if(!function_exists('set_active_route')) {
 	function set_active_route($route) {
 		return Route::is($route) ? 'active' : '';
+	}
+}
+
+if(!function_exists('number_of_messages')) {
+	function number_of_messages() {
+		$count = Message::count();
+		if($count == 1) {
+			return $count. ' message envoyé';
+		}
+		return $count != 0 ? $count. ' messages envoyés' : 'Pas de messages';
 	}
 }
